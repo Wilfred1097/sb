a7beed7e7bbaa71fc8aa18f818b71eb630c40982
Wilfredo D. Catalan <catalanwilfredo97@gmail.com>
2025-03-20 21:37:22 +0800

    initial commit

---
+++ assets/config/config.php
@@ -0,0 +1,19 @@
+<?php 
+
+//Sandbox Credentials
+// define('PAYPAL_CLIENT_ID', 'Ae4PLMpm7sfu3m1UW7MeJ8zWOOkefWz3IiH6KLXDkyfyO3pKfwzxvQ3ALE1bfNSPEGzBu8ak3bQ-kOYt'); 
+// define('PAYPAL_SECRET_ID', 'EKcfAEQ5F8JIo5SKtnUTjeAVcRlys_0-BDSlbqVP9hwo2qOXdXph74mlkEeLQUHT7h4649JBOW0e9veH');
+// define('PAYPAL_API', 'https://api.sandbox.paypal.com/v1/');
+
+//Live Credentials
+define('PAYPAL_CLIENT_ID', 'AWNqmqnKTqHfjtDyo3BBlOPVMwmUZhFznyYWUlCVmnrduNRw9m5IqNA1F6dKSrxsvq53pw07YjJXf3It');
+define('PAYPAL_SECRET_ID', 'EDcCyg7MwjWJkynJyW90FoI7PlztTPXtndyzyLiS-EEo3E1LBPlRiUMQk-i1Tcwp8smLNOlSLXtT5YTx');
+define('PAYPAL_API', 'https://api.paypal.com/v1/');
+
+define('APP_URL', 'http://localhost/Gp/success.php');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'strategic_beast');
+
+?>
